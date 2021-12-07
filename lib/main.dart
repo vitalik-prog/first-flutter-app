781e7841350f77bdbe525cd2ca828133eeec4391
Vitaliy <270vitalik270@gmail.com>
2021-12-07 11:21:35 +0200

    added img widget && svg widget with plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyStatelessWidget());
@@ -17,22 +18,47 @@ class MyStatelessWidget extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.grey[900],
         ),
-        body: RichText(
-          text: const TextSpan(
-              style: TextStyle(
-                  fontSize: 20,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.black,
-                  fontFamily: "Notable"),
-              children: <TextSpan>[
-                TextSpan(text: 'Hello '),
-                TextSpan(style: TextStyle(color: Colors.red), children: <TextSpan>[
-                  TextSpan(text: 'New '),
-                  TextSpan(text: 'Big '),
-                  TextSpan(text: 'World ', style: TextStyle(decoration: TextDecoration.underline)),
-                ]),
-                TextSpan(text: '!!'),
-              ]),
+        body: ListView(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(bottom: 10.0),
+              child: RichText(
+                text: const TextSpan(
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.black,
+                        fontFamily: "Notable"),
+                    children: <TextSpan>[
+                      TextSpan(text: 'Hello '),
+                      TextSpan(
+                          style: TextStyle(color: Colors.red),
+                          children: <TextSpan>[
+                            TextSpan(text: 'New '),
+                            TextSpan(text: 'Big '),
+                            TextSpan(
+                                text: 'World ',
+                                style: TextStyle(
+                                    decoration: TextDecoration.underline)),
+                          ]),
+                      TextSpan(text: '!!!'),
+                    ]),
+
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 20.0),
+              child: SvgPicture.asset('assets/img/play.svg', alignment: Alignment.bottomCenter,),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 20.0),
+              child: Image.network('https://source.unsplash.com/random/'),
+            ),
+            Image.asset('assets/img/podcast5.jpg', width: 100, height: 100,),
+            const Image(
+              image: AssetImage('assets/img/podcast1.jpg'),
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => {},
