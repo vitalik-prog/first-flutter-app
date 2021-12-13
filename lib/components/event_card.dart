48277cb27fed7a6ef731ec0304768080e220761e
Vitaliy <270vitalik270@gmail.com>
2021-12-13 18:03:35 +0200

    added stateful widget

--- lib/components/event_card.dart
+++ lib/components/event_card.dart
@@ -1,7 +1,7 @@
 import 'package:first_flutter_app/models/events.dart';
 import 'package:flutter/material.dart';
 
-class EventCard extends StatelessWidget {
+class EventCard extends StatefulWidget {
   const EventCard({
     Key? key,
     required this.singleEvent,
@@ -10,7 +10,21 @@ class EventCard extends StatelessWidget {
   final Event singleEvent;
 
   @override
+  State<EventCard> createState() => _EventCardState();
+}
+
+class _EventCardState extends State<EventCard> {
+  var _isEnabled = true;
+
+  @override
+  void initState() {
+    super.initState();
+    print("init State");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("build");
     return Card(
         color: Colors.cyan,
         elevation: 10,
@@ -18,18 +32,21 @@ class EventCard extends StatelessWidget {
         margin: const EdgeInsets.symmetric(vertical: 12),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         child: ListTile(
-          title: Text(singleEvent.name, style: const TextStyle(fontSize: 30)),
-          subtitle: Text("${singleEvent.location} ${singleEvent.startDateTime}", style: const TextStyle(fontSize: 15)),
+          enabled: _isEnabled,
+          title: Text(widget.singleEvent.name, style: const TextStyle(fontSize: 30)),
+          subtitle: Text("${widget.singleEvent.location} ${widget.singleEvent.startDateTime}", style: const TextStyle(fontSize: 15)),
           leading: const Icon(
             Icons.local_activity,
             size: 40,
           ),
-          trailing: const IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: null,
+          trailing: IconButton(
+            icon: _isEnabled ? const Icon(Icons.lock_open) : const Icon(Icons.lock_outline),
+            onPressed: () => setState(() {
+              _isEnabled = !_isEnabled;
+            }),
           ),
-          onTap: () => print("${singleEvent.name}  tap"),
-          onLongPress: () => print("${singleEvent.name}  longPress"),
+          onTap: () => print("${widget.singleEvent.name}  tap"),
+          onLongPress: () => print("${widget.singleEvent.name}  longPress"),
         ));
   }
 }
